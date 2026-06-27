37f29f00e768f67afe93bcc3e3847ae24c94cd4a
junaid <zaroonjunaidaslam@gmail.com>
2026-06-27 22:44:42 +0500

    update

---
+++ myProject21/accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+from .forms import RegistrationForm, LoginForm
+
+# Create your views here.
+def 
