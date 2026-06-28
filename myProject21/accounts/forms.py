9fd9d67e00f0f857c465386e9eea23d83f084dd3
junaid <zaroonjunaidaslam@gmail.com>
2026-06-28 21:36:33 +0500

    Authentication

--- myProject21/accounts/forms.py
+++ myProject21/accounts/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationsForm
+from django.contrib.auth.forms import UserCreationForm
 
-class RegistrationForm(UserCreationsForm):
+class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
--- myProject21/accounts/views.py
+++ myProject21/accounts/views.py
@@ -1,9 +1,43 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import messages
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
-from .forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm
 
 # Create your views here.
-def 
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful. and logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Registration failed. Please correct the errors below.')
+    else:
+        form = RegistrationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login successful.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'accounts/login.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('login')
+
+@login_required(login_url='login')
+def dashboard_views(request):
+    return render(request, 'accounts/dashboard.html')
