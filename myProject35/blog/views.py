9d4fff2c7d1fb47be109cceada026f269e9037d6
junaid <zaroonjunaidaslam@gmail.com>
2026-07-13 20:38:14 +0500

    Per-view Cache

---
+++ myProject35/blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from . models import UserList
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+from django.http import HttpResponse
+
+
+@cache_page(30)
+def users_list(request):
+    print("Fetching user list form database")
+    users = UserList.objects.all()
+    return render(request, 'users.html', {'users' :users})
+
+def clear_catch(request):
+    cache.clear()
+    return HttpResponse('Cache Cleared')
+
+
+
+
+
