d84ac06a3526edcc8d4f49f2f1cb2e564cf2bd63
junaid <zaroonjunaidaslam@gmail.com>
2026-07-14 21:06:41 +0500

    Template Fragment Cache

--- myProject35/blog/views.py
+++ myProject35/blog/views.py
@@ -5,11 +5,16 @@ from django.core.cache import cache
 from django.http import HttpResponse
 
 
-@cache_page(30)
+# @cache_page(30)
+# def users_list(request):
+#     print("Fetching user list form database")
+#     users = UserList.objects.all()
+#     return render(request, 'users.html', {'users' :users})
+
 def users_list(request):
-    print("Fetching user list form database")
+    print('Fetching user form database')
     users = UserList.objects.all()
-    return render(request, 'users.html', {'users' :users})
+    return render(request,'users.html',{'users':users})
 
 def clear_catch(request):
     cache.clear()
