fa7f63077b503e1eabeffec081f9ab18baf88901
junaid <zaroonjunaidaslam@gmail.com>
2026-07-19 21:34:19 +0500

    POST

--- apiProject02/api/views.py
+++ apiProject02/api/views.py
@@ -3,9 +3,18 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Student
 from .serializers import StudentSerializers
+from rest_framework import status
 
 @api_view(['GET'])
 def get_students(request):
     students = Student.objects.all()
     serializer = StudentSerializers(students, many=True)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def add_student(request):
+    serializer = StudentSerializers(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
