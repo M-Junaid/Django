3f75f76b18c067f350c0db2acf9c25e5deb4a8c7
junaid <zaroonjunaidaslam@gmail.com>
2026-07-21 22:56:25 +0500

    Put,Patch

--- apiProject02/api/urls.py
+++ apiProject02/api/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('students/', views.get_students,name="get_students"),
-    path('students/add', views.add_student, name='add_student'),   
+    path('students/add', views.add_student, name='add_student'), 
+    path('students/update/<int:pk>/', views.update_student, name='update_student')  
 ]
 
 
