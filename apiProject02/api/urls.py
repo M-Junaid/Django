a47f58a30fe9fb5d9d1efdec918b34e7a10807fa
junaid <zaroonjunaidaslam@gmail.com>
2026-07-22 22:15:39 +0500

    Delete

--- apiProject02/api/urls.py
+++ apiProject02/api/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('students/', views.get_students,name="get_students"),
     path('students/add', views.add_student, name='add_student'), 
-    path('students/update/<int:pk>/', views.update_student, name='update_student')  
+    path('students/update/<int:pk>/', views.update_student, name='update_student'),
+    path('students/delete/<int:pk>/', views.delete_student, name='delete_student'), 
 ]
 
 
