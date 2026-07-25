d3afeaabdd2308120bde499fe87e3b2ef5939081
junaid <zaroonjunaidaslam@gmail.com>
2026-07-25 22:41:49 +0500

    CRUD using GenericAPIView + Mixins

--- apiProject03/api/serializers.py
+++ apiProject03/api/serializers.py
@@ -4,4 +4,4 @@ from .models import Student
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        field = "__all__"
+        fields = "__all__"
--- apiProject03/api/urls.py
+++ apiProject03/api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import StudentAPI
+from .views import StudentListCreateAPI,StudentRetrieveUpdateDeleteAPI
+#from .views import StudentAPI
+
+# urlpatterns = [
+#     path('student/', StudentAPI.as_view()),
+#     path('students/<int:pk>/', StudentAPI.as_view())
+# ]
 
 urlpatterns = [
-    path('student/', StudentAPI.as_view()),
-    path('students/<int:pk>/', StudentAPI.as_view())
+    path("student/", StudentListCreateAPI.as_view()),
+    path('student/<int:pk>/', StudentRetrieveUpdateDeleteAPI.as_view()),
 ]
