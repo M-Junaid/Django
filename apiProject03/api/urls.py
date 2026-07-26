f08c4681bdab4fde0c9057febd65c1f9606d92e3
junaid <zaroonjunaidaslam@gmail.com>
2026-07-26 21:24:11 +0500

    ModesViewSet & Routers

--- apiProject03/api/urls.py
+++ apiProject03/api/urls.py
@@ -1,13 +1,26 @@
-from django.urls import path
-from .views import StudentListCreateAPI,StudentRetrieveUpdateDeleteAPI
+# from django.urls import path
+# from .views import StudentListCreateAPI,StudentRetrieveUpdateDeleteAPI
 #from .views import StudentAPI
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import StudentViewSet 
+
+router = DefaultRouter()
+router.register('students', StudentViewSet, basename='student')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
+
+
 
 # urlpatterns = [
 #     path('student/', StudentAPI.as_view()),
 #     path('students/<int:pk>/', StudentAPI.as_view())
 # ]
 
-urlpatterns = [
-    path("student/", StudentListCreateAPI.as_view()),
-    path('student/<int:pk>/', StudentRetrieveUpdateDeleteAPI.as_view()),
-]
+# urlpatterns = [
+#     path("student/", StudentListCreateAPI.as_view()),
+#     path('student/<int:pk>/', StudentRetrieveUpdateDeleteAPI.as_view()),
+# ]
