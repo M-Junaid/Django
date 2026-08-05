bd322b5c968d275211c9f4e393b38100d9523eb2
junaid <zaroonjunaidaslam@gmail.com>
2026-08-05 16:35:12 +0500

    fucntion base

--- student_api/students/urls.py
+++ student_api/students/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import student_list, create_student
+from .views import student_list, student_detail
+
 
 urlpatterns = [
     path("students/", student_list),
     # path("students/create/", create_student),
+    path("students/<int:id>/", student_detail),
 ]
--- student_api/students/views.py
+++ student_api/students/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -20,3 +21,37 @@ def student_list(request):
             return Response(serializer.data, status=201)
 
         return Response(serializer.errors, status=400)
+
+
+@api_view(["GET", "PUT","PATCH", "DELETE"])
+def student_detail(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if request.method == "GET":
+        serializer = StudentSerializer(student)
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        serializer = StudentSerializer(student, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+    if request.method == "PATCH":
+        serializer = StudentSerializer(
+        student,
+        data=request.data,
+        partial=True
+    )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+    if request.method == "DELETE":
+        student.delete()
+        return Response(status=204)
+
+    
