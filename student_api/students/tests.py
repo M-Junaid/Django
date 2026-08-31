84589b90c865e0d8f81efc648ea0be1d214e3ca2
junaid <zaroonjunaidaslam@gmail.com>
2026-08-31 13:53:21 +0500

    test

--- student_api/students/tests.py
+++ student_api/students/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
 
+from rest_framework.test import APITestCase, APIClient
+# APITestCase -> Test API requests automatically
+class StudentAPITest(APITestCase):
+    # setUp() --> "prepare the API client"
+    #Before each test, prepare an API client. 
+    def setUp(self):
+        # APIClient gives your test a way to act like a client making API requests.
+        self.client = APIClient()
 
+    def test_unauthenticated_user_cannot_access_student_list(self):
+        response = self.client.get('/students/')
+        self.assertEqual(response.status_code, 401) 
